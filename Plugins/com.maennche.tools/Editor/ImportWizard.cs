a8bed8f7af2ddbde7a1daa06f6e22141ac51c898
Christopher Maennche <Christopher@maennche.com>
2022-11-06 14:45:07 -0600

    1.0.2

--- Plugins/com.maennche.tools/Editor/ImportWizard.cs
+++ Plugins/com.maennche.tools/Editor/ImportWizard.cs
@@ -1,16 +1,15 @@
-﻿using System.Diagnostics;
+﻿using System;
 using System.IO;
-using System.Text;
+using System.Net;
 using UnityEditor;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 namespace cmaennche {
     public class ImportWizard : EditorWindow {
-        [MenuItem("Tools/Maennche/Importer")]
+        [MenuItem("Tools/Maennche/Import Wizard")]
         private static void ShowWindow() {
             var window = GetWindow<ImportWizard>();
-            window.titleContent = new GUIContent("Importer");
+            window.titleContent = new GUIContent("Import Wizard");
             window.Show();
         }
 
@@ -28,12 +27,22 @@ namespace cmaennche {
             // Debug.Log("There are " + sdkPackages.Length + " sdk unitypackages in this directory.");
             
             EditorGUILayout.LabelField("Custom Packages ( " + customPackages.Length + " )", EditorStyles.boldLabel);
-            // GUILayout.BeginHorizontal();
-            // EditorGUILayout.PrefixLabel();
-            // if (GUILayout.Button("Import")) {
-            //     Process.Start();
-            // }
-            // GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.PrefixLabel("Basic FPS Controller");
+            if (GUILayout.Button("Import")) {
+                string githubToken = "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";
+                var url = "https://github.com/cmaennche/Game-Assets/archive/FPSController-Basic.unitypackage";
+                var path = Path.Combine(Application.dataPath, "FPSController.unitypackage");
+                
+                using (var client = new System.Net.Http.HttpClient()) {
+                    var credentials = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}:", githubToken);
+                    credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(credentials));
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", credentials);
+                    var contents = client.GetByteArrayAsync(url).Result;
+                    System.IO.File.WriteAllBytes(path, contents);
+                }
+            }
+            GUILayout.EndHorizontal();
             
             
             EditorGUILayout.LabelField("SDK Packages ( " + sdkPackages.Length + " )", EditorStyles.boldLabel);
